ffea5d1f6e8d30d51da95a7fc678039867372843
francepants <franceline90@gmail.com>
2020-07-31 20:50:16 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create(
+    {name: "Stephen King", genre: "Horror", bio: "Bio details go here"}
+)
